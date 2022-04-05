42fb2f830b9a1d0ad3468060cb18e364342d9a54
MF049 <midofox199@gmail.com>
2022-04-06 00:44:06 +0200

    main code

---
+++ Photoshop.cpp
@@ -0,0 +1,57 @@
+// Program: photoshop
+// Purpose: Demonstrate use of bmplip for handling
+//          bmp colored and grayscale images
+//          Program load a gray image and store in another file
+// Author:  Mohammad FathElrahman Osman
+// Date:    30 March 2018
+// Version: 1.0
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+void loadImage ();
+void saveImage ();
+void doSomethingForImage ();
+
+int main()
+{
+  loadImage();
+  saveImage();
+  return 0;
+}
+
+//_________________________________________
+void loadImage () {
+   char imageFileName[100];
+
+   // Get gray scale image file name
+   cout << "Enter the source image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   readGSBMP(imageFileName, image);
+}
+
+//_________________________________________
+void saveImage () {
+   char imageFileName[100];
+
+   // Get gray scale image target file name
+   cout << "Enter the target image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   writeGSBMP(imageFileName, image);
+}
+
+//_________________________________________
+// write your code function under this line pleas
+//and write your names in the top of the program

65f2867f930c0fb3ccab33a46ad9002308767191
MF049 <midofox199@gmail.com>
2022-04-07 07:10:47 +0200

    Update Photoshop.cpp

--- Photoshop.cpp
+++ Photoshop.cpp
@@ -17,12 +17,14 @@ unsigned char image[SIZE][SIZE];
 
 void loadImage ();
 void saveImage ();
+void mirror ();
  
 
 int main()
 {
    auto choose;
   loadImage();
+  //under this line cout your filters numbers ... the varible "choose" its for the user to pick up filter number
    cin >> choose;
     switch (choose) {
         // edit this statement with your function number and mention the function inside its case
@@ -30,10 +32,13 @@ int main()
         case 2:
         case 3:
         case 4:
+        case "a" :
+          mirror();
+           break;
   saveImage();
   return 0;
 }
-
+}
 //_________________________________________
 void loadImage () {
    char imageFileName[100];
@@ -63,3 +68,42 @@ void saveImage () {
 //_________________________________________
 // write your code function under this line pleas
 //and write your names in the top of the program
+void mirror (){
+ string choise;
+ 
+   cout << "Mirror (l)eft, (r)ight, (u)pper, (d)own side? : ";
+   cin >> choise;
+   switch (choise)
+   {
+   case  "l":
+    for (int i = 0; i < SIZE; i++) {
+       for (int j = SIZE; j > 0; j--) {
+           image[i][j] = image[i][SIZE-j];     
+       }
+        }
+      break;
+   case "r" :
+     for (int i = 0; i < SIZE; i++) {
+         for (int j = 0; j < SIZE; j++) {
+           image[i][j] = image[i][SIZE-j];   
+   
+
+   }
+} 
+break;
+   case "u" :
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+         image[SIZE-i][SIZE-j] = image[i][SIZE-j]; 
+      }
+       }
+       break;
+   case "d" : 
+      for (int i = 0; i < SIZE; i++) {
+         for (int j = 0; j < SIZE; j++) {
+            image[i][j] =image[SIZE-i][j];     
+         }
+          }
+          break;
+   }
+
